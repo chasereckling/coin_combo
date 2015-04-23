4cad7dd9b1d74710a5ed6ca32d6133e44cf19efb
Jesse James & Chase Reckling <student@epicodus.com>
2015-04-23 09:49:40 -0700

    Write new test to handle change from division based method to subtraction looping

--- spec/coin_combo_spec.rb
+++ spec/coin_combo_spec.rb
@@ -5,10 +5,14 @@ describe('String#coin_combo') do
   it('takes a value and returns the amount of change remaining') do
     expect(("50").coin_combo()).to(eq("0"))
   end
-  it('divides the counter value by the value of a quarter to return amount of change remaining') do
-    expect(("50").coin_combo()).to(eq("0"))
-  end
-  it('divides the counter value by the value of a quarter and then a dime to return the amount of change remaining') do
-    expect(("60").coin_combo()).to(eq('0'))
+  # it('divides the counter value by the value of a quarter to return amount of change remaining') do
+  #   expect(("50").coin_combo()).to(eq("0"))
+  # end
+  # it('divides the counter value by the value of a quarter and then a dime to return the amount of change remaining') do
+  #   expect(("60").coin_combo()).to(eq('0'))
+  # end
+  # removed tests as we changed the method plan
+  it('return a value for the amount of change remaining given quarters') do
+    expect(("50")).coin_combo()).to(eq("0"))
   end
 end
