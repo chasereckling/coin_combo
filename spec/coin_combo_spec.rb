19b51b265b169630997113bb2a531c3705452ac3
Jesse James & Chase Reckling <student@epicodus.com>
2015-04-23 09:38:44 -0700

    Write 3rd test, request division of counter value by quarter and dime

--- spec/coin_combo_spec.rb
+++ spec/coin_combo_spec.rb
@@ -5,7 +5,10 @@ describe('String#coin_combo') do
   it('takes a value and returns the amount of change remaining') do
     expect(("50").coin_combo()).to(eq("0"))
   end
-  it('divides the given value by the value of a quarter to return amount of change remaining') do
+  it('divides the counter value by the value of a quarter to return amount of change remaining') do
     expect(("50").coin_combo()).to(eq("0"))
+  end
+  it('divides the counter value by the value of a quarter and then a dime to return the amount of change remaining') do
+    expect(("60").coin_combo()).to(eq('0'))
   end
 end
