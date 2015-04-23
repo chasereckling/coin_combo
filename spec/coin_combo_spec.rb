01e50dfb159956aedcebde72bf4014b6b8cc3a18
Jesse James & Chase Reckling <student@epicodus.com>
2015-04-23 09:17:23 -0700

    Write first unit test spec for accepting and returning a value

--- spec/coin_combo_spec.rb
+++ spec/coin_combo_spec.rb
@@ -2,7 +2,7 @@ require('rspec')
 require('coin_combo')
 
 describe('String#coin_combo') do
-  it('') do
-    expect().to(eq())
+  it('takes a Fixnum value and returns the amount of change remaining') do
+    expect((50).coin_combo()).to(eq(50))
   end
 end
